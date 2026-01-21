0a607511531bee8808f23c62e902732e6cdffdb0
Sahil Sinha <sahilsinha.dar@gmail.com>
2026-01-21 19:27:52 +0530

    refactor: updated default api version to 2026-01
    - updated the default API version to 2026-01

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub struct ShopifyClient {
 
 impl ShopifyClient {
     pub fn new(shop_url: String, access_token: String, api_version: Option<String>) -> Self {
-        let api_version = api_version.unwrap_or("2024-07".to_string());
+        let api_version = api_version.unwrap_or("2026-01".to_string());
         let shop_url_arc = Arc::new(shop_url);
         let api_version_arc = Arc::new(api_version);
         let access_token_arc = Arc::new(access_token);
